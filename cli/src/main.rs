c2fedaa6d1587aeec08d9d32bf86db112a2ec4ff
Sahil Dhanju <sahildhanju1@gmail.com>
2020-10-24 00:13:32 +0100

    Got the cli to hit an endpoint

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,13 @@
+// use quicli::prelude::*;
+// use structopt::Structop;
+
+mod idkrequest;
+
+// #[derive(StructOpt)]
+// struct Cli {
+//     request: String,
+// }
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", idkrequest::make_request("test"))
 }
