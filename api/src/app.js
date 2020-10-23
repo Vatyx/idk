b2b3ad3cc837d05c5e95441d10f4e7c35996e43d
Muin Momin <momin.muin@gmail.com>
2020-10-23 17:53:32 -0500

    Set up docker stuff for cloud run

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios').default;
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.get("/", async (req, res) => {
+    res.status(200).json({});
+});
 
 app.listen(port, () => {
     console.log(`Listening on ${port}...`);
