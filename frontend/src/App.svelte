c65175d6a7089f8d26b5cca087fdb9dcfb5d0a6d
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-24 12:42:51 -0400

    change

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,35 @@
-<script>
-	export let name;
+<script lang="ts">
+	import { createClient } from '@supabase/supabase-js'
+	const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYwMzUwMjQ5MSwiZXhwIjoxOTE5MDc4NDkxfQ.z94jL3kJSO8awkd3h_yvdhsX5lsgmStFmQZShRBzoi4'
+	const SUPABASE_URL = "https://bhlpgnzrnvrchycujafn.supabase.co"
+	const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+	export let name: string;
+	let value: string;
+	supabase
+		.from('Prompt')
+		.select('*').then(r => {
+		value = r.body[0].content;
+		})
+
+	const saveContent = () => {
+		supabase
+		.from('Prompt')
+		.update({ content: value })
+		.eq('id', 1).then(r => console.log(r.body))
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<textarea cols="8" row="8" bind:value></textarea>
+	<button on:click={saveContent}>save</button>
 </main>
 
 <style>
+	textarea {
+		height: 200px;
+		width: 800px;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
