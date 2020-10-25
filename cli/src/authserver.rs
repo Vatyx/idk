000ec96a5c31bcd9e3eeb84d24e596f5cc4c1a52
Sahil Dhanju <sahildhanju1@gmail.com>
2020-10-25 02:27:11 +0000

    it WOOOOOOOOOOOOOOOOOORKS

---
+++ cli/src/authserver.rs
@@ -0,0 +1,30 @@
+use warp::Filter;
+use tokio::sync::mpsc;
+
+pub async fn start_server() -> String {
+
+    // let (tx, mut rx) = watch::channel::<String>("".to_string());
+
+    let (signal_sender, mut signal_receiver) = mpsc::channel::<String>(1);
+    let (data_sender, mut data_receiver) = mpsc::channel::<String>(1);
+
+    let with_signal = warp::any().map(move || signal_sender.clone());
+    let with_data = warp::any().map(move || data_sender.clone());
+
+    let routes = warp::any().and(warp::path::param()).and(with_signal).and(with_data).and_then(move |name: String, tx: mpsc::Sender<String>, dx: mpsc::Sender<String>| async move { 
+        tx.clone().send("".to_string()).await.ok();
+        dx.clone().send(name).await.ok();
+        Ok::<_, warp::reject::Rejection>("Done")
+    });
+    
+        // dataSender.send(name);
+
+    let (_addr, server) = warp::serve(routes).bind_with_graceful_shutdown(([127, 0, 0, 1], 8765), async move {
+        signal_receiver.recv().await.unwrap();
+    });
+
+    async {
+        tokio::task::spawn(server).await.ok();
+        data_receiver.recv().await.unwrap()
+    }.await
+}
